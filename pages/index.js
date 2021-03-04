be11530ab48f35420523f52e4b802905a12fdf23
ryand67 <rwdevenney@gmail.com>
2021-03-04 15:24:16 -0500

    form

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import Head from 'next/head'
-import '../styles/Home.module.css'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Budgeting App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <h1 className={styles.title}>Budgeting App</h1>
+      <form className={styles.inputForm}>
+        <input type="number" placeholder="Amount" name="amount" className={styles.amountInput} id=""/>
+        <input type="text" placeholder="Description" name="description" className={styles.descInput} id=""/>
+        <button type="submit" className={styles.addButton}>Add</button>
+      </form>
     </div>
   )
 }
